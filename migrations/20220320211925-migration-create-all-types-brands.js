e0fc19a5ca5e15008b0231225f6f42858ef8b6c4
Alex Polivanov <agios987@gmail.com>
2022-04-16 20:05:36 +0300

    IntShop-server-part6

--- migrations/20220320211925-migration-create-all-types-brands.js
+++ migrations/20220320211925-migration-create-all-types-brands.js
@@ -14,19 +14,19 @@ module.exports = {
     console.log('***users = ', users[0].name);
     console.log('***usersLength = ', users.length);
 
-     for ( let i = 0; i <= users.length - 1; i++ ) {
+/*     for ( let i = 0; i <= users.length - 1; i++ ) {
        await userM.create({name: users[i].name, email: users[i].email}, function (err, results) {
 
         // console.log(results);
        });
 
-     };
+     };*/
 
 //       await Baskets.create({name: 'basket1'});
 //       await Baskets.create({name: 'basket2'});
        // await Baskets.update({})
 
-    /*
+
     await Type.create({ name: 'Холодильники'}),
     await Type.create({ name: 'Телефоны'}),
 
@@ -37,7 +37,7 @@ module.exports = {
     await Device.create({ name: 'Samsung2', price: 11000, BrandId:1, TypeId:1, img: '3b866d08-a930-44e1-9921-2b726b77c3dd.jpg' })
     await Device.create({ name: 'Apple1', price: 12000, BrandId:2, TypeId:2, img: '3ac8fb99-7fff-445d-a21d-a4424e08d974.jpg' })
     await Device.create({ name: 'Apple2', price: 14000, BrandId:2, TypeId:2, img: '6431697c-4ecb-4a05-adff-33cac72a30b7.jpg' })
-*/
+
     },
 
   async down (queryInterface, Sequelize) {
